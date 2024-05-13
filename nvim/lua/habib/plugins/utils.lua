dfbfb184a8e07c8e66d8b90a3b6abd9e018d976f
holocure <nanosyalesia@gmail.com>
2024-05-13 03:58:30 +0300

    v0.1

---
+++ nvim/lua/habib/plugins/utils.lua
@@ -0,0 +1,17 @@
+return {
+	-- lorem ipsum
+	{
+		"derektata/lorem.nvim",
+		config = function()
+			local lorem = require("lorem")
+			lorem.setup({
+				sentenceLength = "mixedShort",
+				comma = 1,
+			})
+		end,
+	},
+	-- rainbow_csv
+	{
+		"mechatroner/rainbow_csv",
+	},
+}
---
+++ zsh/nvim/init.lua
@@ -0,0 +1,1 @@
+require("habib.core")
